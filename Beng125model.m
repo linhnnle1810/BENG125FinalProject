bde95164ed2c3618587db81d4048a154aa1264a4
allenle619 <50930889+allenle619@users.noreply.github.com>
2019-06-02 22:40:35 -0700

    Update Beng125model.m

--- Beng125model.m
+++ Beng125model.m
@@ -2,21 +2,21 @@
 alpha=1.62e-4;
 beta=1.52e-4;
 gamma=3.00e-2;
-N=2.00e2;
-I0=1;
+N=200;
+I0=2;
 S0=N-I0;
 
 %Ode Solver
 f=@(t,y) [-alpha*y(1)*y(2) ; ((alpha-beta)*y(1)*y(2))+((beta*N-gamma-(beta*y(2))*y(2)))];
-trange=[0,2500];
+trange=[0,5000];
 initials=[S0 I0];
 [time, infect]=ode45(f,trange,initials);
 
-%Plot
+%Plot of Views Per Day
 plot(time, infect(:,2),'k-')
 xlabel('Time(Day)')
 ylabel('Search Volume Index')
-title('Search Text= "Blog"')
+title('Search Text = "Blog"')
 ylim([0 12]);
-xlim([0 2500]);
+xlim([0 5000]);
 legend('I(t)')
